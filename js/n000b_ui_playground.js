c5bda66da27aaeb8066c92927010e42ba18d84f3
herrbasan <david@raum.com>
2024-12-16 22:15:23 +0100

    added animation playground

--- js/n000b_ui_playground.js
+++ js/n000b_ui_playground.js
@@ -17,6 +17,7 @@ import global_audio from '../nui/nui_audio_context.js';
 import audioVisualizer from "../nui/nui_audio_visualizer.js";
 import mediaPlayer from "../nui/nui_media_player.js";
 import cms_page_editor from '../nui/nui_cms_page_editor.js';
+import dragSlider from '../nui/nui_drag_slider.js';
 
 let nuip = {};
 let g = {};
@@ -164,6 +165,11 @@ nuip.init = function(){
 						name:'NUI Icons',
 						id:'icons',
 						icon: ut.icon('arrow')
+					},
+					{
+						name:'NUI Animation',
+						id:'animation',
+						icon: ut.icon('label')
 					}
 				]
 			}
@@ -254,6 +260,9 @@ function go(obj){
 	}
 	if(obj.page == 'misc' && obj.id == 'icons'){
 		init_page_icons();
+	}
+	if(obj.page == 'misc' && obj.id == 'animation'){
+		init_page_animation();
 	}
 
 	if(!ut.el(id)){
@@ -830,6 +839,255 @@ function init_page_icons(){
 	}
 	g.content.appendChild(g.page_icons);
 }
+
+
+function init_page_animation(){
+	if(g.page_animation) { return;}
+	g.page_animation = ut.htmlObject(/*html*/`
+		<main id="page_misc_animation">
+			<section>
+				<article>
+					<div class="nui-card info">
+						<div class="ani_time"><label>Time</label><div>0</div></div>
+						<div class="ani_state"><label>State</label><div>none</div></div>
+					</div>
+					<div class="nui-card">
+						<div class="animation_demo">${ut.icon('sync')}</div>
+					</div>
+					<div class="nui-card">
+						<div class="ani_timeline"><div class="prog"></div></div>
+					</div>
+				</article>
+			</section>
+		</main>
+	`);
+	let content = g.page_animation.el('.animation_demo');
+	let info_time = g.page_animation.el('.ani_time div');
+	let info_state = g.page_animation.el('.ani_state div');
+	let timeline = g.page_animation.el('.ani_timeline');
+	let prog = g.page_animation.el('.ani_timeline .prog');
+
+	content.css({height:'100%', display:'flex', 'align-items':'center', 'justify-content':'center'})
+	let img = content.el('svg');
+	img.css({width:'50%', height:'50%'});
+	let a = ani({
+		el: img,
+		props:[ 
+			{ offset:0, 'transform-origin':'center center', scale:0.2, rotate:45, opacity:0, easing:'quart.out'},
+			{ offset:0.4, scale:1, rotate:0, opacity:1, easing:'quart.in' },
+			{ offset:1, scale:1.2, rotate:-45, opacity:0}
+		], 
+		options:{ 
+			duration: 1500,
+			fill:'forwards'
+		},
+		update:update,
+		cb:console.log,
+		events:console.log
+	})
+
+	function update(e){
+		info_time.innerText = parseInt(e.currentTime);
+		info_state.innerText = e.target.state;
+		prog.css({width:`${e.progress * 100}%`});
+	}
+
+	content.addEventListener('click', () => a.play(0));
+	g.content.appendChild(g.page_animation);
+	let drag = dragSlider(timeline, (e) => {
+		if(e.type == 'move'){
+			a.pause();
+			a.animation.currentTime = e.prozX * a.duration;
+			prog.css({width:`${e.prozX * 100}%`});
+		}
+		else if(e.type == 'end'){
+			a.play(a.animation.currentTime);
+		}
+		else {
+			console.log(e.type)
+		}
+		
+	});
+}
+
+function ani(obj){
+	console.log(parseProps(obj.props));
+    let keyframes = new KeyframeEffect(obj.el, parseProps(obj.props), obj.options)
+    let mation = new Animation(keyframes, document.timeline);
+    let ani = {};
+	ani.animation = mation;
+	ani.duration = obj.options.duration;
+	reset();
+    if(!obj.paused) { loop(); play(); }
+    
+    mation.onfinish = finish;
+    mation.onremove = remove;
+	mation.oncancel = cancel;
+	
+    ani.play = play;
+    ani.pause = pause;
+
+    function play(time){ 
+		events('start');
+		if(time != undefined){mation.currentTime = time};
+		ani.loop = true; 
+		mation.play(); 
+		loop(); 
+	}
+
+	function cancel(){
+		events('cancel');
+		reset();
+	}
+
+    function pause(){ events('pause'); mation.pause(); }
+
+    function remove(e){ events('remove'); ani.loop = false; }
+    
+    function finish(e){
+        events('finished')
+        if(obj.cb) { obj.cb(ani); }
+    }
+
+	function reset(){
+		ani.lastTime = 0;
+		ani.currentTime = 0;
+		ani.state = 'init';
+		ani.loop = false;
+	}
+
+    function events(msg){
+        ani.state = msg;
+        if(obj.events) { obj.events({target:ani, msg:msg});}
+    }
+
+	function update(){
+		if(obj.update) { 
+			obj.update({target:ani, currentTime:ani.currentTime, progress:ani.currentTime / obj.options.duration}); 
+		}
+	}
+
+    function loop(){
+        if(mation?.currentTime != ani.lastTime){
+            ani.currentTime = mation.currentTime;
+            ani.lastTime = ani.currentTime;
+            update();
+        }
+        //if(ani.loop){ requestAnimationFrame(loop); }
+		requestAnimationFrame(loop);
+    }
+
+	function parseProps(props){
+		if(!(props instanceof Array)){
+			return parseProp(props);
+		}
+		let out = [];
+		for(let i=0; i<props.length; i++){
+			out.push(parseProp(props[i]))
+		}
+		return out;
+	}
+	
+	function parseProp(props){
+		let tmpl = {
+			tx:{template:'translateX', transform:true, defaultMetric:'px'},
+			ty:{template:'translateY', transform:true, defaultMetric:'px'},
+			tz:{template:'translateZ', transform:true, defaultMetric:'px'},
+			scaleX:{template:'scaleX', transform:true, defaultMetric:false},
+			scaleY:{template:'scaleY', transform:true, defaultMetric:false},
+			scale:{template:'scale', transform:true, defaultMetric:false},
+			rotate:{template:'rotate', transform:true, defaultMetric:'deg'},
+			x:{template:'left', defaultMetric:'px'},
+			y:{template:'top', defaultMetric:'px'},
+			width:{defaultMetric:'px'},
+			height:{defaultMetric:'px'},
+		}
+	
+		let ease = {
+			sine:{
+				in:'cubic-bezier(0.13, 0, 0.39, 0)',
+				out:'cubic-bezier(0.61, 1, 0.87, 1)',
+				inOut:'cubic-bezier(0.36, 0, 0.64, 1)'
+			},
+			quad:{
+				in:'cubic-bezier(0.11, 0, 0.5, 0)',
+				out:'cubic-bezier(0.5, 1, 0.89, 1)',
+				inOut:'cubic-bezier(0.44, 0, 0.56, 1)'
+			},
+			cubic:{
+				in:'cubic-bezier(0.32, 0, 0.67, 0)',
+				out:'cubic-bezier(0.33, 1, 0.68, 1)',
+				inOut:'cubic-bezier(0.66, 0, 0.34, 1)'
+			},
+			quart:{
+				in:'cubic-bezier(0.5, 0, 0.75, 0)',
+				out:'cubic-bezier(0.25, 1, 0.5, 1)',
+				inOut:'cubic-bezier(0.78, 0, 0.22, 1)'
+			},
+			quint:{
+				in:'cubic-bezier(0.64, 0, 0.78, 0)',
+				out:'cubic-bezier(0.22, 1, 0.36, 1)',
+				inOut:'cubic-bezier(0.86, 0, 0.14, 1)'
+			},
+			expo:{
+				in:'cubic-bezier(0.7, 0, 0.84, 0)',
+				out:'cubic-bezier(0.16, 1, 0.3, 1)',
+				inOut:'cubic-bezier(0.9, 0, 0.1, 1)'
+			},
+			circ:{
+				in:'cubic-bezier(0.55, 0, 1, 0.45)',
+				out:'cubic-bezier(0, 0.55, 0.45, 1)',
+				inOut:'cubic-bezier(0.85, 0.09, 0.15, 0.91)'
+			},
+			back:{
+				in:'cubic-bezier(0.36, 0, 0.66, -0.56)',
+				out:'cubic-bezier(0.34, 1.56, 0.64, 1)',
+				inOut:'cubic-bezier(0.8, -0.4, 0.5, 1)'
+			}
+		}
+	
+		let out = {transform:''}
+		for(let key in props){
+			if(key in tmpl){
+				if(tmpl[key].transform){
+					out.transform += tmpl[key].template + '(' + dm(tmpl[key].defaultMetric, props[key]) + ') '
+				} else {
+					if(tmpl[key].template){
+						out[tmpl[key].template] = dm(tmpl[key].defaultMetric, props[key]);
+					}
+					else {
+						out[key] = dm(tmpl[key].defaultMetric, props[key]);
+					}
+				}
+			}
+			else if(key == 'ease' || key == 'easing'){
+				out.easing = props[key];
+				let deep = ut.deep_get(ease,props[key]);
+				if(deep != undefined){
+					out.easing = deep;
+				}
+			}
+			else {
+				if(key == 'transform') { props[key] += ' '}
+				out[key] = props[key];
+			}
+		}
+		if(out.transform.length < 2){
+			delete out.transform;
+		}
+		return out;
+	
+		function dm(metric, value){
+			if(!metric){ return value; }
+			if(isNaN(value)){ return value; }
+			return value.toString() + metric;
+		}
+	}
+
+    return ani;
+}
+
+
 
 
 nuip.randomImage = function(res){
